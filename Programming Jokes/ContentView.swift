22a8a23c2b10399a9534aa571ca682e5623edabd
Daisy Guerrero <dguerrero1@bsd220.org>
2022-02-02 10:24:18 -0600

    Got API key

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -26,12 +26,7 @@ struct ContentView: View {
                })
     }
     func getJokes() {
-           jokes.append(Joke(setup: "Why do programmers always get Christmas and Halloween mixed up?",
-                             punchline: "Because DEC 25 = OCT 31"))
-           jokes.append(Joke(setup: "How did the programmer die in the shower?",
-                             punchline: "She followed the shampoo bottle instructions: Lather. Rinse. Repeat."))
-           jokes.append(Joke(setup: "There are 10 types of people in the world",
-                             punchline: "Those who understand binary and those who don’t."))
+        let apiKey = "?rapidapi-key=001b5c5e5emsh7fc081c3e3e9f4bp1b46f4jsn80c50e3dfd2c"
        }
 }
 
